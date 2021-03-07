910fec6dd38f03e05a142fc49aa7a9f1ac070f9f
Vinyl Smyk <80162687+ScratchyDot@users.noreply.github.com>
2021-03-07 21:07:03 +0100

    Setup added

---
+++ spearlux.js
@@ -0,0 +1,12 @@
+//Spearlux Theme
+module.exports = {
+    theme: {
+        colors: {
+            white: "#E0E0E0",
+            black: "#464646"
+        },
+        fontFamily: {
+            sans: ['Roboto', 'sans-serif'],
+        },
+    }
+}
