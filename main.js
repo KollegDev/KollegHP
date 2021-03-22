e08ea701f71dfeed14153ec5cd9e9f6160cf3436
p01nt1 <67297284+p01nt1@users.noreply.github.com>
2021-03-22 16:50:57 +0100

    Mobile navbar added
    * Adjustments to the height
    * Navbar added
    * Overlap at the footer fixed
    
    Subsequent fine adjustments possible

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Init");
+
+document.getElementById("hamburger").onclick = function toggleMenu() {
+    const navToggle = document.getElementsByClassName("toggle");
+    for (let i = 0; i < navToggle.length; i++) {
+      navToggle.item(i).classList.toggle("hidden");
+    }
+  };
