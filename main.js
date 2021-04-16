f8f45d0ebf6e9296031503c470ea971f2a2f7774
Vinyl Smyk <80162687+ScratchyDot@users.noreply.github.com>
2021-04-16 21:05:55 +0200

    Dark Theme, Transitions, Overall fixes added
    +Added Dark Theme to the following pages:
    - index.html
    - contact.html
    - jobs.html
    - projectDescription.html
    - projects.html
    - subjects.html
    - women.html
    
    + Barba Transitions added to supported subpages
    Credits to @Member1773
    
    +Overall Layout/Style fixes

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import barba from "@barba/core";
-
-barba.init({});
-
 console.log("Init");
 setInterval(animateTitle, 500);
+
 
 function animateTitle() {
   if (document.title == "Informatik_") {
@@ -13,11 +10,13 @@ function animateTitle() {
   }
 }
 
+
 function NavToggle(elements) {
   for (let i = 0; i < elements.length; i++) {
     elements.item(i).classList.toggle("hidden");
   }
 }
+
 
 document.getElementById("hamburger").onclick = function toggleMenu() {
   NavToggle(document.getElementsByClassName("toggle"));
